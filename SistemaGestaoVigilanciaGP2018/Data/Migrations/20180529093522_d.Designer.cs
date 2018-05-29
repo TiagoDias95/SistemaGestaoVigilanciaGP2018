1227513c0ed5a6b67bd7bb873a3be670a95a5b0d
Tiago Dias <Tiago Dias@MSI>
2018-05-29 10:37:46 +0100

    alterações a nivel do model do utilizador, com migrações

--- SistemaGestaoVigilanciaGP2018/Data/Migrations/20180529093522_d.Designer.cs
+++ SistemaGestaoVigilanciaGP2018/Data/Migrations/20180529093522_d.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SistemaGestaoVigilanciaGP2018.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180529093522_d")]
+    partial class d
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SistemaGestaoVigilanciaGP2018/Data/Migrations/20180529093522_d.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaGestaoVigilanciaGP2018.Data.Migrations
+{
+    public partial class d : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumeroDocente",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroDocente",
+                table: "AspNetUsers");
+        }
+    }
+}
