d316337714e71f3d2f14d40bdef14b25553bf9b9
Tiago Dias <Tiago Dias@MSI>
2018-06-14 00:02:56 +0100

    migrações

---
+++ SistemaGestaoVigilanciaGP2018/Data/Migrations/20180613194508_ProtecaoDados.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaGestaoVigilanciaGP2018.Data.Migrations
+{
+    public partial class ProtecaoDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ProtecaoDados",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProtecaoDados",
+                table: "AspNetUsers");
+        }
+    }
+}
